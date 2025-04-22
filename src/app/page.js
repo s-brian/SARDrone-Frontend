447ac9ecc532deb32642188f91ac324af19e5868
Mercy Kim <mercy@Mercys-MacBook-Air.local>
2025-04-21 23:17:26 -0700

    added home page images

--- src/app/page.js
+++ src/app/page.js
@@ -9,25 +9,25 @@ export default function Home() {
     <main className="min-h-screen bg-white overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24">
         <AnimatedText
-          title="i changed this :P"
-          description="this is what i want to put here mercy200"
-          imageUrl="/placeholder.svg?height=600&width=800"
+          title="our mission"
+          description="We aim to provide an affordable and easily accessible drone to assist non-funded search and rescue missions."
+          imageUrl="/group.jpg"
           imageAlt="Drone illustration"
           imagePosition="right"
         />
 
         <AnimatedText
-          title="Title"
-          description="Description"
-          imageUrl="/placeholder.svg?height=600&width=800"
+          title="the problem"
+          description="Most search and rescue operations are run by volunteers with little equipment, leading to delays ranging from from hours to days when finding the location of lost people."
+          imageUrl="/mountain.jpg"
           imageAlt="Drone illustration"
           imagePosition="left"
         />
 
         <AnimatedText
-          title="Title"
-          description="Description"
-          imageUrl="/placeholder.svg?height=600&width=800"
+          title="key features"
+          description="Camera, Autonomous flight, AI image analyzation and detection"
+          imageUrl="/drone.jpg"
           imageAlt="Drone illustration"
           imagePosition="right"
         />
